875bf93cf63362fc13ee63c81d051d465c4fa3b1
vardhan2000 <vardhan.av2000@gmail.com>
2022-11-29 05:24:17 +0530

    completed testing of option 1

--- Artifact/src/test/java/TestRunAppWhileTwiceWithOption1.java
+++ Artifact/src/test/java/TestRunAppWhileTwiceWithOption1.java
@@ -50,4 +50,52 @@ class TestRunAppWhileTwiceWithOption1 {
 
 		assertEquals(new ArrayList<>(), orderList);
 	}
+
+
+	@Test
+	void testRunAppCreateOrderCustomPizza() {
+		Order.orderCount = 1;
+		Pizza.currentId = 1;
+		Drink.drinkCount = 1;
+
+		List<Order> orderList = PizzaParlour.runApp("1\nPizza\nM\nCustom\nJalapenos\nMushrooms\nUndo\nDone\n1\nCheckout\nDelivery\n309 Pizza Parlour\n6\n","Menu.txt");
+		System.out.println(orderList);
+
+		Order.orderCount = 1;
+		Pizza.currentId = 1;
+		Drink.drinkCount = 1;
+
+		ArrayList<Pizza> p = new ArrayList<>();
+		ArrayList<String> toppings = new ArrayList<>();
+		toppings.add("Jalapenos");
+		p.add(PizzaFactory.makeCustomPizza('M', 1, toppings));
+		ArrayList<Order> o = new ArrayList<>();
+		Order firstOrder = OrderFactory.getOrder(p, new ArrayList<>(), "309 Pizza Parlour", "Pickup");
+		o.add(firstOrder);
+
+		System.out.println(o);
+		assertEquals(true, o.toString().equals(orderList.toString()));
+	}
+
+	@Test
+	void testRunAppCreateOrderDrinks() {
+		Order.orderCount = 1;
+		Pizza.currentId = 1;
+		Drink.drinkCount = 1;
+
+		List<Order> orderList = PizzaParlour.runApp("1\nDrink\nM\n4\n2\nCheckout\nFoodora\n309 Pizza Parlour\n6\n","Menu.txt");
+		System.out.println(orderList);
+
+		Order.orderCount = 1;
+		Pizza.currentId = 1;
+		Drink.drinkCount = 1;
+
+		ArrayList<Drink> d = new ArrayList<>();
+		d.add(new Drink("Pepsi",2));
+		ArrayList<Order> o = new ArrayList<>();
+		Order firstOrder = OrderFactory.getOrder(new ArrayList<>(), d, "309 Pizza Parlour", "Pickup");
+		o.add(firstOrder);
+
+		assertEquals(true, o.toString().equals(orderList.toString()));
+	}
 }
